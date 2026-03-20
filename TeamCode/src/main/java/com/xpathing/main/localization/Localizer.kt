acf2d898b637fcbcaf9ca982ab678c84fddc8747
sohum-arora <sohumarora28@gmail.com>
2026-03-20 20:48:56 +0530

    initial commit

---
+++ TeamCode/src/main/java/com/xpathing/main/localization/Localizer.kt
@@ -0,0 +1,40 @@
+package com.xpathing.main.localization
+
+import com.xpathing.main.follower.paths.Pose
+import com.xpathing.util.math.Vector
+import java.lang.annotation.Inherited
+
+@MustBeDocumented
+@Inherited
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Pathing
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Inherited
+@MustBeDocumented
+annotation class Localizer
+
+abstract class LocalizerBase {
+    protected var lastPosition: Pose = Pose(0.0, 0.0, 0.0)
+    protected var lastVelocity: Vector = Vector(0.0, 0.0)
+
+    var currentPosition: Pose = Pose(0.0, 0.0, 0.0)
+    var currentVelocity: Vector = Vector(0.0, 0.0)
+    var currentAcceleration: Vector = Vector(0.0, 0.0)
+
+    abstract fun initLocalizer(deviceName : String)
+    abstract fun update()
+    abstract fun setPose(pose: Pose)
+
+    protected fun updateKinematics() {
+        currentVelocity = Vector(
+            currentPosition.x - lastPosition.x,
+            currentPosition.y - lastPosition.y
+        )
+        currentAcceleration = currentVelocity.subtract(lastVelocity)
+        lastVelocity = currentVelocity.copy()
+        lastPosition = currentPosition.copyPose()
+    }
+}
